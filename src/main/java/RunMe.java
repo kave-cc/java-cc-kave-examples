8827a98f35672e71a2f3b6373e379a5f1862ec7f
Sebastian Proksch <development@mail.proks.ch>
2017-07-21 14:34:04 +0200

    Polish example.

--- src/main/java/RunMe.java
+++ src/main/java/RunMe.java
@@ -14,67 +14,20 @@
  * the License.
  */
 
-
-import java.io.File;
-import java.util.Set;
-
-import cc.kave.commons.model.events.CommandEvent;
-import cc.kave.commons.model.events.IDEEvent;
-import cc.kave.commons.model.events.completionevents.CompletionEvent;
-import cc.kave.commons.utils.io.Directory;
-import cc.kave.commons.utils.io.IReadingArchive;
-import cc.kave.commons.utils.io.ReadingArchive;
+import examples.GettingStarted;
 
 public class RunMe {
 
 	/*
-	 * download the interaction data and untip it into the root of this project
+	 * download the interaction data and unzip it into the root of this project
 	 * (at the level of the pom.xml). Unpack it, you should now have a folder
-	 * that includes a bunch of folders that resemble dates and that contain
-	 * .zip files. Each .zip represents all events that we have collected for a
-	 * specific user, the folder represents the first day when the user uploaded
-	 * data.
+	 * that includes a bunch of folders that have dates as names and that
+	 * contain .zip files.
 	 */
-	public static String root = "Events-170301";
+	public static String eventsDir = "Events-170301";
 
 	public static void main(String[] args) {
 
-		System.out.printf("working in folder %s\n", new File(root).getAbsolutePath());
-
-		int userNum = 0;
-		for (String userZip : findUserZips()) {
-			System.out.printf("#### (%d) processing user zip: %s #####\n", userNum++, userZip);
-
-			try (IReadingArchive ra = new ReadingArchive(new File(root, userZip))) {
-				while (ra.hasNext()) {
-					process(ra.getNext(IDEEvent.class));
-				}
-				System.out.println("...\n");
-			}
-		}
-	}
-
-	private static void process(IDEEvent e) {
-		// please bear with me for the casting, I will implement the visitor
-		// pattern once I have time... :)
-
-		if (e instanceof CommandEvent) {
-			CommandEvent ce = (CommandEvent) e;
-			System.out.printf("found a CommandEvent (id: %s)\n", ce.getCommandId());
-		} else if (e instanceof CompletionEvent) {
-			CompletionEvent compe = (CompletionEvent) e;
-			System.out.printf("found a CompletionEvent (was triggered in: %s)\n",
-					compe.context.getSST().getEnclosingType().getFullName());
-		} else {
-			System.out.printf("found an %s (triggered at: %s)\n", e.getClass().getSimpleName(), e.getTriggeredAt());
-		}
-
-		// these are just some examples, please explore the API to see what kind
-		// of data is available
-	}
-
-	public static Set<String> findUserZips() {
-		Directory dir = new Directory(root);
-		return dir.findFiles(s -> s.endsWith(".zip"));
+		new GettingStarted(eventsDir).run();
 	}
 }
---
+++ src/main/java/examples/GettingStarted.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2016 University of Zurich
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package examples;
+
+import java.io.File;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+import cc.kave.commons.model.events.CommandEvent;
+import cc.kave.commons.model.events.IDEEvent;
+import cc.kave.commons.model.events.completionevents.CompletionEvent;
+import cc.kave.commons.model.ssts.ISST;
+import cc.kave.commons.utils.io.Directory;
+import cc.kave.commons.utils.io.IReadingArchive;
+import cc.kave.commons.utils.io.ReadingArchive;
+
+/**
+ * Simple example that shows how the dataset can be opened, all users identified,
+ * and all contained events deserialized.
+ */
+public class GettingStarted {
+
+	private String eventsDir;
+
+	public GettingStarted(String eventsDir) {
+		this.eventsDir = eventsDir;
+	}
+
+	public void run() {
+
+		System.out.printf("looking (recursively) for events in folder %s\n", new File(eventsDir).getAbsolutePath());
+
+		/*
+		 * Each .zip that is contained in the eventsDir represents all events
+		 * that we have collected for a specific user, the folder represents the
+		 * first day when the user uploaded data.
+		 */
+		Set<String> userZips = findUserZips();
+
+		for (String userZip : userZips) {
+			System.out.printf("\n#### processing user zip: %s #####\n", userZip);
+
+			// open the .zip file ...
+			try (IReadingArchive ra = new ReadingArchive(new File(eventsDir, userZip))) {
+				// ... and iterate over content.
+				while (ra.hasNext()) {
+					/*
+					 * within the userZip, each stored event is contained as a
+					 * single file that contains the Json representation of a
+					 * subclass of IDEEvent.
+					 */
+					IDEEvent e = ra.getNext(IDEEvent.class);
+
+					// the events can then be processed individually
+					process(e);
+				}
+			}
+		}
+	}
+
+	/*
+	 * will recursively search for all .zip files in the eventsDir. The paths
+	 * that are returned are relative to the eventsDir.
+	 */
+	public Set<String> findUserZips() {
+		return new Directory(eventsDir).findFiles(s -> s.endsWith(".zip"));
+	}
+
+	/*
+	 * if you review the type hierarchy of IDEEvent, you will realize that
+	 * several subclasses exist that provide access to context information that
+	 * is specific to the event type.
+	 * 
+	 * To access the context, you should check for the runtime type of the event
+	 * and cast it accordingly.
+	 * 
+	 * As soon as I have some more time, I will implement the visitor pattern to
+	 * get rid of the casting. For now, this is recommended way to access the
+	 * contents.
+	 */
+	private void process(IDEEvent e) {
+
+		if (e instanceof CommandEvent) {
+			process((CommandEvent) e);
+		} else if (e instanceof CompletionEvent) {
+			process((CompletionEvent) e);
+		} else {
+			/*
+			 * CommandEvent and Completion event are just two examples, please
+			 * explore the type hierarchy of IDEEvent to find other types and
+			 * review their API to understand what kind of context data is
+			 * available.
+			 * 
+			 * We include this "fall back" case, to show which basic information
+			 * is always available.
+			 */
+			processBasic(e);
+		}
+
+	}
+
+	private void process(CommandEvent ce) {
+		System.out.printf("found a CommandEvent (id: %s)\n", ce.getCommandId());
+	}
+
+	private void process(CompletionEvent e) {
+		ISST snapshotOfEnclosingType = e.context.getSST();
+		String enclosingTypeName = snapshotOfEnclosingType.getEnclosingType().getFullName();
+
+		System.out.printf("found a CompletionEvent (was triggered in: %s)\n", enclosingTypeName);
+	}
+
+	private void processBasic(IDEEvent e) {
+		String eventType = e.getClass().getSimpleName();
+		ZonedDateTime triggerTime = e.getTriggeredAt();
+
+		System.out.printf("found an %s that has been triggered at: %s)\n", eventType, triggerTime);
+	}
+}
